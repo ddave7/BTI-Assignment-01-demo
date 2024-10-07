23e669a544f8e204841ef82672c189ddb8275d70
ddave7 <ddave7@myseneca.ca>
2024-10-06 22:50:58 -0400

    App setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'); // "require" the Express module
+const path = require('path');
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/home.html'));
+  });
+
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/about.html'));
+  });
+
+// start the server on the port and output a confirmation to the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
