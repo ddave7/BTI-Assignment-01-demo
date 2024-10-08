59c8666b6632183860b7d9382dc5bd3a721454cd
ddave7 <ddave7@myseneca.ca>
2024-10-08 19:22:31 -0400

    public.css creation

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express'); // "require" the Express module
+const express = require('express'); 
 const path = require('path');
-const app = express(); // obtain the "app" object
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
-app.use(express.static('public'));
+const app = express(); 
+const HTTP_PORT = process.env.PORT || 8080; 
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
-  });
+});
 
-  app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/about.html'));
-  });
+});
 
-// start the server on the port and output a confirmation to the console
+// Start the server
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
